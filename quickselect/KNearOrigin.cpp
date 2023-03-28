1b05641b08d1de167bc9bab728b5e3de336365f2
DwaiB <dwaipayanbasu2@gmail.com>
2023-03-29 00:23:47 +0530

    quick select KNearOrigin (using nth_element)

--- quickselect/KNearOrigin.cpp
+++ quickselect/KNearOrigin.cpp
@@ -9,69 +9,50 @@ struct{
     }
 } logger;
 class Solution {
-public:
-    double euclideanDistance(vector<int> point){
-        double distance = sqrt(point[0]*point[0] + point[1]*point[1]);
-        return distance;
-    }
-    bool nearestToOrigin(vector<int> point1, vector<int> point2) {
-        if(euclideanDistance(point1) < euclideanDistance(point2)){
-            return true;
+    public:
+        static int euclideanDistance(vector<int> point){
+            return point[0]*point[0] + point[1]*point[1];
         }
-        return false;
-    }
-    int positionOfPoint(vector<vector<int>>& points, int start,  int pivot){
-        int i = start-1, j = start;
-        vector<int> temp;
-        while(j<pivot){
-            if(nearestToOrigin(points[j],points[pivot])){
-                i++;
-                temp = points[j];
-                points[j] = points[i];
-                points[i] = temp; 
+        int positionOfPoint(vector<vector<int>>& points, int start,  int pivot){
+            int i = start-1, j = start;
+            vector<int> temp;
+            while(j<pivot){
+                if(euclideanDistance(points[j]) < euclideanDistance(points[pivot])){
+                    i++;
+                    temp = points[j];
+                    points[j] = points[i];
+                    points[i] = temp; 
+                }
+                j++;
             }
-            j++;
+            temp = points[pivot];
+            points[pivot] = points[i+1];
+            points[i+1] = temp;
+            return i+1;
         }
-        temp = points[pivot];
-        points[pivot] = points[i+1];
-        points[i+1] = temp;
-        return i+1;
-    }
-    void qSelectPoints(vector<vector<int>>& points, vector<vector<int>>& result, int k, int start, int end){
-        if(start >= end){ 
-            if(start < k){
-                result[start] = points[start];
+        void qSelectPoints(vector<vector<int>>& points, int k, int start, int end){
+            if(start >= end){ 
+                return;
             }
-            return;
-        }
-        int position = positionOfPoint(points, start, end);
-        if(position == k-1){
-            result[position] = points[position];
-        }
-        else if(position >= k){
-            qSelectPoints(points,result,k,start,position-1);
-        }
-        else{
-            result[position] = points[position];
-            qSelectPoints(points,result,k,position+1,end);
-        }
+            int position = positionOfPoint(points, start, end);
+            if(position == k){
+                return;
+            }
+            else if(position > k){
+                qSelectPoints(points,k,start,position-1);
+            }
+            else{
+                qSelectPoints(points,k,position+1,end);
+            }
 
-    }
-    void DNC(vector<vector<int>>& points, vector<vector<int>>& result, int k,int left,int right){
-        if(k==0){
-            return;
         }
-        qSelectPoints(points,result,k,left,right);
-        DNC(points,result,k-1,left,right);
-    }
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector<vector<int>> result(k);
-        DNC(points,result,k,0,points.size()-1);
-        return result;
-    }
+        vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+            qSelectPoints(points,k-1,0,points.size()-1);
+            return vector<vector<int>>(points.begin(),points.begin()+k);
+        }
 };
 int main(){
-    vector<vector<int>> points {{0,1},{1,0}};
+    vector<vector<int>> points {{3,3},{5,-1},{-2,4}};
     int k = 2;
     Solution sol;
     vector<vector<int>> res = sol.kClosest(points, k);
--- quickselect/KOriginCheck.cpp
+++ quickselect/KOriginCheck.cpp
@@ -1,14 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Solution{
+struct{
+    void print(vector<vector<int>> points){
+        cout << "Vector" << endl;
+        for (vector<int> point : points){
+            cout <<"x,y: "<< point[0] << "," << point[1] << endl;
+        }
+    }
+} logger;
+class Solution {
     public:
-    
-};
+        static int euclideanDistance(vector<int> point){
+            return point[0]*point[0] + point[1]*point[1];
+        }
+        
+        vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
 
+            nth_element(points.begin(),points.begin()+k,points.end(),[this](const vector<int> point1,const vector<int> point2){
+                return euclideanDistance(point1) < euclideanDistance(point2);
+            });
+            return vector<vector<int>>(points.begin(),points.begin()+k);
+        }
+};
 int main(){
-    vector<int> point {1,2};
-    double p = 25;
-    double dist = sqrt(p);
-    cout << dist << endl;
+    vector<vector<int>> points {{3,3},{5,-1},{-2,4}};
+    int k = 2;
+    Solution sol;
+    vector<vector<int>> res = sol.kClosest(points, k);
+    logger.print(res);
     return 0;
 }
