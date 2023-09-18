4461eb14bfdb117280d3924215b7905b5e09cd13
DwaiB <dwaipayanbasu2@gmail.com>
2023-09-18 08:27:54 +0530

    Backtrack

--- Java/CodeTrix/ListToArray.java
+++ Java/CodeTrix/ListToArray.java
@@ -1,19 +1,9 @@
 package Java.CodeTrix;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class ListToArray{
     public static void main(String[] args) {
-        Map<Integer,String> map = new HashMap<Integer,String>();
-        map.put(1,"a");
-        map.put(2,"b");
-        map.put(3,"c");
-        map.put(4,"d");
-        map.put(5,"e");
-        map.put(6,"f");
-        List<Integer> keys = map.keySet().stream().sorted().toList();
-        Integer [] timestamps =  keys.toArray(new Integer[keys.size()]);
+        StringBuilder b = new StringBuilder("1Q21");
         
+        System.out.println(b.toString().replaceAll("[0-9]", "."));
     }
 }
--- LeetCode/backtrack/NQueens.java
+++ LeetCode/backtrack/NQueens.java
@@ -1,135 +1,153 @@
 package LeetCode.backtrack;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class NQueens {
     List<List<String>> res;
 
-    public void addToList(char[][] dp, int n) {
+    public void addToList(StringBuilder[] board, int n) {
         List<String> list = new ArrayList<String>();
-        String sb = "";
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (dp[i][j] == 'Q') {
-                    sb += "Q";
-                } else {
-                    sb += ".";
-                }
-            }
-            list.add(sb);
-            sb = "";
+            list.add(board[i].toString());
         }
-        if(!res.contains(list))
+        if (!res.contains(list))
             res.add(list);
     }
 
-    public boolean checkPosition(char[][] dp, int n, int row, int col) {
+    public void fillPoss(StringBuilder[] board, int n, int row, int col,char pLetter,char letter) {
         int i = 0, j = 0;
+        
         // check row and column
         for (i = 0; i < n; i++) {
-            if (dp[row][i] == 'Q' || dp[i][col] == 'Q') {
-                return false;
+            if (board[i].charAt(col) == pLetter) {
+                board[i].setCharAt(col,letter);
+            }
+            if (board[row].charAt(i) == pLetter) {
+                board[row].setCharAt(i,letter);
             }
         }
-        // left upper diagonal
         i = row - 1;
         j = col - 1;
-        while (i >= 0 && j >= 0) {
-            if (dp[i][j] == 'Q') {
-                return false;
+        while (i >= 0 && j >=0 ) {
+            if (board[i].charAt(j) == pLetter) {
+                board[i].setCharAt(j, letter);
             }
             i--;
             j--;
+        }
+        i = row +1;
+        j = col +1;
+        while (i <n && j < n) {
+            if (board[i].charAt(j) == pLetter) {
+                board[i].setCharAt(j, letter);
+            }
+            i++;
+            j++;
         }
         // right upper diagonal
         i = row - 1;
         j = col + 1;
         while (i >= 0 && j < n) {
-            if (dp[i][j] == 'Q') {
-                return false;
+            if (board[i].charAt(j) == pLetter ) {
+                board[i].setCharAt(j, letter);
             }
             i--;
             j++;
         }
-        // left lower diagonal
         i = row + 1;
         j = col - 1;
-        while (i < n && j >= 0) {
-            if (dp[i][j] == 'Q') {
-                return false;
+        while (i <n && j >=0) {
+            if (board[i].charAt(j) == pLetter) {
+                board[i].setCharAt(j, letter);
             }
             i++;
             j--;
         }
-        // right lower diagonal
-        i = row + 1;
-        j = col + 1;
-        while (i < n && j < n) {
-            if (dp[i][j] == 'Q') {
-                return false;
-            }
-            i++;
-            j++;
-        }
-        return true;
     }
 
-    public void backtrack(char[][] dp,boolean [][] exis, int n, int queens) {
+    public void backtrack(StringBuilder[] board, int n, int queens) {
         if (queens > n) {
             return;
         } else if (queens == n) {
-            addToList(dp, n);
+            addToList(board, n);
+            return;
         }
         for (int i = 0; i < n; i++) {
+            if (board[i].indexOf("Q") != -1) {
+                continue;
+            }
             for (int j = 0; j < n; j++) {
-                if (checkPosition(dp, n, i, j) && !exis[i][j]) {
-                    dp[i][j] = 'Q';
-                    // print(dp, n);
-                    exis[i][j] = true;
-                    backtrack(dp,exis, n, queens + 1);
-                    exis[i][j] = false;
-                    dp[i][j] = '.';
+
+                if (board[i].charAt(j) == '.') {
+                    board[i].setCharAt(j, 'Q');
+                    fillPoss(board,n,i,j,'.',(char)(queens+48));
+                    // print(board,n);
+                    backtrack(board, n, queens + 1);
+                    fillPoss(board,n,i,j,(char)(queens+48),'.');
+                    board[i].setCharAt(j, '.');
                 }
             }
         }
     }
 
-    public void print(char[][] dp, int n) {
-        int i = 0, j = 0;
-        System.out.print("\n[");
-        for (i = 0; i < n; i++) {
-            System.out.print("\"");
-            for (j = 0; j < n; j++) {
-                System.out.print(dp[i][j]);
-            }
-            System.out.print("\",");
-        }
-        System.out.print("]");
-    }
-
     public List<List<String>> solveNQueens(int n) {
         res = new ArrayList<List<String>>();
-        char[][] dp = new char[n][n];
-        boolean [][] exis = new boolean[n][n];
-        for(int i = 0; i < n; i++) {
-            Arrays.fill(dp[i],'.');
-            Arrays.fill(exis[i], false);
+        StringBuilder[] board = new StringBuilder[n];
+        for (int i = 0; i < n; i++) {
+            board[i] = new StringBuilder(".".repeat(n));
         }
-        backtrack(dp,exis, n, 0);
+        backtrack(board, n, 0);
         return res;
     }
 
-    public static void main(String[] args) {
-        NQueens object = new NQueens();
-        List<List<String>> list = object.solveNQueens(4);
-        for (List<String> rows : list) {
-            System.out.print("\n[");
-            for (String row : rows) {
-                System.out.print("\"" + row + "\",");
-            }
-            System.out.print("]");
-        }
-    }
+
+
+    //  public void print(StringBuilder[] board, int n) {
+    //     int i = 0, j = 0;
+    //     for (i = 0; i < n; i++) {
+    //         System.out.print("___");
+    //     }
+    //     for (i = 0; i < n; i++) {
+    //         System.out.print("\n| ");
+    //         for (j = 0; j < n; j++) {
+    //             System.out.print(board[i].charAt(j)+" ");
+    //         }
+    //         System.out.print(" |");
+    //     }
+    //     System.out.println();
+    //     for (i = 0; i < n; i++) {
+    //         System.out.print("___");
+    //     }
+    //     System.out.println();
+    // }
+    // public static void main(String[] args) {
+    //     NQueens object = new NQueens();
+    //     // List<List<String>> list = object.solveNQueens(3);
+    //     // for (List<String> rows : list) {
+    //     //     System.out.print("\n[");
+    //     //     for (String row : rows) {
+    //     //         System.out.print("\"" + row + "\",");
+    //     //     }
+    //     //     System.out.print("]");
+    //     // }
+    //     // String [] board = {"....","..Q.","....","...."};
+    //     // StringBuilder [] sb = new StringBuilder[4];
+    //     // for(int i = 0; i < 4; i++) {
+    //     //     sb[i] = new StringBuilder(board[i]);
+    //     // }
+    //     // object.print(sb,4);
+    //     // int row,col;
+    //     // Scanner sc = new Scanner(System.in);
+    //     // try{
+    //     //     while(true){
+    //     //         System.out.println("Enter row and column");
+    //     //         row = sc.nextInt();
+    //     //         col = sc.nextInt();
+    //     //         System.out.println("Exist =>"+object.checkQueen(sb, 4, row, col));
+    //     //     }
+    //     // }finally{
+    //     //     sc.close();
+    //     // }
+    // }
 }
